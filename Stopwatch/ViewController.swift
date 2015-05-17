5fb7bc119ee46d10a51ec99fe785789d2173407b
Branko Ajzele <ajzelegmail.com>
2015-05-17 09:57:52 +0200

    Simple stopwatch

--- Stopwatch/ViewController.swift
+++ Stopwatch/ViewController.swift
@@ -10,6 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var timer = NSTimer()
+    var counter = 0
+    
+    @IBOutlet weak var time: UILabel!
+    
+    
+    
+    @IBAction func pause(sender: AnyObject) {
+        timer.invalidate()
+    }
+    
+    @IBAction func stop(sender: AnyObject) {
+        timer.invalidate()
+        counter = 0
+        time.text = "\(counter)"
+    }
+    
+    @IBAction func play(sender: AnyObject) {
+        
+        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("updateTime"), userInfo: nil, repeats: true)
+        
+    }
+    
+    func updateTime() {
+        counter++
+        time.text = "\(counter)"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
